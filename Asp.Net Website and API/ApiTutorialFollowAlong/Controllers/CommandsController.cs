8c08a8a88a4cdb4bfd6bc9d8ac4786dbdc9b0a05
VaneHiro <mac.vanessa@hotmail.it>
2021-04-10 20:44:47 +0200

    inizialize repository and commandscontroller

---
+++ Asp.Net Website and API/ApiTutorialFollowAlong/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    // I could just write api/commands
+    // But with this method, if the class name changes the route doesn't
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        // GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ Asp.Net Website and API/ApiTutorialFollowAlong/Data/MockCommanderRepo.cs
@@ -0,0 +1,27 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id = 0, HowTo = "Break an egg", Line = "Have fun", Platform = "Mic and stage"},
+                new Command{Id = 1, HowTo = "Paint a wall", Line = "Get a paint brush", Platform = "Your room"},
+                new Command{Id = 2, HowTo = "Cut bread", Line = "Get a knife", Platform = "Cutting board"}
+            };
+            return commands;
+        }
+         
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Break an egg", Line="Have gun", Platform="Mic and stage"};
+        }
+    }
+}
